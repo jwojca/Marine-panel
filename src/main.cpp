c2df9934cda9420c83b6523198948bcaf995ee25
jwojca <63160362+jwojca@users.noreply.github.com>
2023-01-03 10:47:33 +0100

    3state button read function with read

--- src/main.cpp
+++ src/main.cpp
@@ -39,7 +39,7 @@ PCF8574 pcf8574(ADRESS);
 
 bool read2State(uint8_t pin, bool printOn)
 {
-  uint8_t val = pcf8574.digitalRead(P0);
+  uint8_t val = pcf8574.digitalRead(pin);
   bool state;
   if(val == 0)
     state = true;
@@ -54,6 +54,27 @@ bool read2State(uint8_t pin, bool printOn)
  return state;
 }
 
+uint8_t read3State(uint8_t pin1, uint8_t pin2, bool printOn)
+{
+  uint8_t val1 = pcf8574.digitalRead(pin1);
+  uint8_t val2 = pcf8574.digitalRead(pin2);
+  uint8_t state;
+  if(val1 == 1 && val2 == 1)
+    state = 0;
+  else if(val2 == 0)
+    state = 1;
+  else
+    state = 2;
+
+   if (printOn)
+  {
+    Serial.print(state);
+    Serial.print("\n"); 
+  }  
+
+  return state;
+}
+
 void setup()
 {
 	Serial.begin(9600);
@@ -61,6 +82,7 @@ void setup()
 
 	// Set pinMode to OUTPUT
 	pcf8574.pinMode(P0, INPUT);
+  pcf8574.pinMode(P1, INPUT);
 
 	Serial.print("Init pcf8574...");
 	if (pcf8574.begin())
@@ -71,7 +93,7 @@ void setup()
 
 void loop()
 {
-	read2State(P0, false);
+	read3State(P0, P1, true);
   delay(100);
 }
 
