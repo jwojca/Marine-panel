353c1ebd976f6341f7d23246f16c77654f4c71b1
jwojca <63160362+jwojca@users.noreply.github.com>
2023-01-04 10:58:34 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 #include <SPI.h>
 #include <Ethernet.h>
 // nastavení MAC adresy
@@ -114,6 +115,36 @@ void setup() {
 }
 
 void loop() {
+=======
+#include <Wire.h>
+#include "SSD1306Wire.h"
+
+// for 128x64 displays:
+SSD1306Wire display(0x3c, 8, 9);  // ADDRESS, SDA, SCL
+// By default SH1106Wire set I2C frequency to 700000, you can use set either another frequency or skip setting the frequency by providing -1 value
+// SH1106Wire(0x3c, SDA, SCL, GEOMETRY_128_64, I2C_ONE, 400000); //set I2C frequency to 400kHz
+// SH1106Wire(0x3c, SDA, SCL, GEOMETRY_128_64, I2C_ONE, -1); //skip setting the I2C bus frequency
+
+
+void setup() {
+  String text1 = "Frequency: 50 Hz";
+  String text2 = "Power: 600 kW";
+  String text3 = "Voltage: 1000 V";
+  // put your setup code here, to run once:
+  display.init();
+  display.setContrast(255);
+  display.setFont(ArialMT_Plain_16);
+  display.drawStringMaxWidth(0,0, display.getWidth(),text1);
+  display.drawStringMaxWidth(0,20, display.getWidth(),text2);
+  display.drawStringMaxWidth(0,40, display.getWidth(),text3);
+  display.display();
+  delay(10000);
+  display.clear();
+  display.setFont(ArialMT_Plain_10); 
+}
+
+void loop() {
+>>>>>>> parent of 24d3ccd (First modbus test)
   int bigRadius = 31;
   int smallRadius = 25;
   display.drawString((display.getWidth()/2)-15, (display.getHeight()/2)-10, "0 MW");
@@ -190,5 +221,9 @@ void loop()
 
   delay(5000);           // wait 5 seconds for next scan
 }
+<<<<<<< HEAD
+*/
+>>>>>>> parent of 24d3ccd (First modbus test)
+=======
 */
 >>>>>>> parent of 24d3ccd (First modbus test)
