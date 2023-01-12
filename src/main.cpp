e06a96965891b90e1c4039d2fe6e91b63dc0e45e
jwojca <h.wojcik@seznam.cz>
2023-01-12 10:42:20 +0100

    Modbus master created, respondes to ping, dont have any slave

--- src/main.cpp
+++ src/main.cpp
@@ -1,141 +1,92 @@
 /*
- Web client
- 
- This sketch connects to a test website (httpbin.org)
- and try to do a GET request, the output is printed
- on Serial
- 
- by Renzo Mischianti <www.mischianti.org>
- 
- https://www.mischianti.org
- 
- */
- 
-#include <SPI.h>
-#include <ModbusEthernet.h>
-#include <Ethernet.h>
- 
-// if you don't want to use DNS (and reduce your sketch size)
-// use the numeric IP instead of the name for the server:
-//IPAddress server(74,125,232,128);  // numeric IP for Google (no DNS)
-//char server[] = "www.google.com";    // name address for Google (using DNS)
-char server[] = "httpbin.org";    // name address for Google (using DNS)
- 
-// Enter a MAC address for your controller below.
-// Newer Ethernet shields have a MAC address printed on a sticker on the shield
-byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
- 
-// Set the static IP address to use if the DHCP fails to assign
-#define MYIPADDR 192,168,1,50
-#define MYIPMASK 255,255,255,0
-#define MYDNS 192,168,1,1
-#define MYGW 192,168,1,1
- 
-// Initialize the Ethernet client library
-// with the IP address and port of the server
-// that you want to connect to (port 80 is default for HTTP):
-EthernetClient client;
- 
-// Variables to measure the speed
-unsigned long beginMicros, endMicros;
-unsigned long byteCount = 0;
-bool printWebData = true;  // set to false for better speed measurement
- 
+  ModbusTCP Client for ClearCode Arduino wrapper
+
+  (c)2021 Alexander Emelianov (a.m.emelianov@gmail.com)
+  https://github.com/emelianov/modbus-esp8266
+
+  This code is licensed under the BSD New License. See LICENSE.txt for more info.
+*/
+
+#define RST 1
+#define PORT 502
+
+#include <Ethernet.h>       // Ethernet library v2 is required
+#include <ModbusAPI.h>
+#include <ModbusTCPTemplate.h>
+
+
+
+class ModbusEthernet : public ModbusAPI<ModbusTCPTemplate<EthernetServer, EthernetClient>> {};
+
+const uint16_t REG = 512;               // Modbus Hreg Offset
+IPAddress server(127, 0, 0, 1);  // Address of Modbus Slave device - need to define!!
+const int32_t showDelay = 5000;   // Show result every n'th mellisecond
+
+bool usingDhcp = true;
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xEE }; // MAC address for your controller
+IPAddress ip(169, 254, 198, 200); // The IP address will be dependent on your local network
+ModbusEthernet mb;               // Declare ModbusTCP instance
+
+
+
 void setup() {
+  
     Serial.begin(9600);
-    delay(1000);
-    Serial.println("Begin Ethernet");
- 
-    // You can use Ethernet.init(pin) to configure the CS pin
-    //Ethernet.init(10);  // Most Arduino shields
-    Ethernet.init(10);   // MKR ETH Shield
-    //Ethernet.init(0);   // Teensy 2.0
-    //Ethernet.init(20);  // Teensy++ 2.0
-    //Ethernet.init(15);  // ESP8266 with Adafruit FeatherWing Ethernet
-    //Ethernet.init(33);  // ESP32 with Adafruit FeatherWing Ethernet
- 
-    if (Ethernet.begin(mac)) { // Dynamic IP setup
-        Serial.println("DHCP OK!");
-    }else{
-        Serial.println("Failed to configure Ethernet using DHCP");
-        // Check for Ethernet hardware present
-        if (Ethernet.hardwareStatus() == EthernetNoHardware) {
-          Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
-          
-        }
-        if (Ethernet.linkStatus() == LinkOFF) {
-          Serial.println("Ethernet cable is not connected.");
-        }
- 
-          IPAddress ip(MYIPADDR);
-          IPAddress dns(MYDNS);
-          IPAddress gw(MYGW);
-          IPAddress sn(MYIPMASK);
-          Ethernet.begin(mac, ip, dns, gw, sn);
-          Serial.println("STATIC OK!");
-    }
-    delay(5000);
- 
- 
-    Serial.print("Local IP : ");
+    uint32_t timeout = 5000;
+    uint32_t startTime = millis();
+    while (!Serial && millis() - startTime < timeout)
+        continue;
+
+    pinMode(RST, OUTPUT);
+    digitalWrite(RST, HIGH);
+    delay(100);
+    digitalWrite(RST, LOW);
+    delay(100);
+    digitalWrite(RST, HIGH);
+    delay(100);
+    Ethernet.init(34);
+
+
+    Ethernet.begin(mac, ip);
+    //server.begin();
+    Serial.print("CLient IP adress: ");
     Serial.println(Ethernet.localIP());
-    Serial.print("Subnet Mask : ");
-    Serial.println(Ethernet.subnetMask());
-    Serial.print("Gateway IP : ");
-    Serial.println(Ethernet.gatewayIP());
-    Serial.print("DNS Server : ");
-    Serial.println(Ethernet.dnsServerIP());
- 
-   Serial.println("Ethernet Successfully Initialized");
-  // if you get a connection, report back via serial:
-  if (client.connect(server, 80)) {
-    Serial.println("Connected!");
-    // Make a HTTP request:
-    client.println("GET /get HTTP/1.1");
-    client.println("Host: httpbin.org");
-    client.println("Connection: close");
-    client.println();
-  } else {
-    // if you didn't get a connection to the server:
-    Serial.println("connection failed");
-  }
-  beginMicros = micros();
+
+    delay(1000); //second to initialize
+    // Make sure the physical link is up before continuing.
+    while (Ethernet.linkStatus() == LinkOFF) {
+        Serial.println("The Ethernet cable is unplugged...");
+        delay(1000);
+    }
+    Serial.println("The Ethernet cable is connected.");
+    Serial.println("Client connecting to Server");
+    mb.connect(server, PORT);
+    if (mb.isConnected(server))
+      Serial.println("connected");     
+    else
+      Serial.println("Connection failed");
 }
- 
+
+uint16_t res = 0;
+uint32_t showLast = 0;
+
+
 void loop() {
-  // if there are incoming bytes available
-  // from the server, read them and print them:
-  int len = client.available();
-  if (len > 0) {
-    byte buffer[80];
-    if (len > 80) len = 80;
-    client.read(buffer, len);
-    if (printWebData) {
-      Serial.write(buffer, len); // show in the serial monitor (slows some boards)
-    }
-    byteCount = byteCount + len;
+if (mb.isConnected(server)) {   // Check if connection to Modbus Slave is established
+    mb.readHreg(server, REG, &res);  // Initiate Read Hreg from Modbus Slave
+  } else {
+    mb.connect(server, PORT);           // Try to connect if not connected
   }
- 
-  // if the server's disconnected, stop the client:
-  if (!client.connected()) {
-    endMicros = micros();
-    Serial.println();
-    Serial.println("disconnecting.");
-    client.stop();
-    Serial.print("Received ");
-    Serial.print(byteCount);
-    Serial.print(" bytes in ");
-    float seconds = (float)(endMicros - beginMicros) / 1000000.0;
-    Serial.print(seconds, 4);
-    float rate = (float)byteCount / seconds / 1000.0;
-    Serial.print(", rate = ");
-    Serial.print(rate);
-    Serial.print(" kbytes/second");
-    Serial.println();
- 
-    // do nothing forevermore:
-    while (true) {
-      delay(1);
-    }
+
+  delay(100);                     // Pulling interval
+  mb.task();                      // Common local Modbus task
+  if (millis() - showLast > showDelay) { // Display register value every 5 seconds (with default settings)
+    showLast = millis();
+    Serial.println(res);
   }
 }
+
+
+
+
+
