df437a6408921d19e10c3ed570b416a97b25a325
jwojca <h.wojcik@seznam.cz>
2023-03-23 11:28:23 +0100

    damper class added

--- lib/Marine_panel/Marine_panel_v2.h
+++ lib/Marine_panel/Marine_panel_v2.h
@@ -94,6 +94,8 @@ static int task = 50;
 
 enum mpMode{Local, Auto};
 enum mpState{Closed, Opened, Failure, Failure2, Stopped, Starting, Stopping, StoppingF, Opening, Closing, Running}; //states for valves and pumps 
+
+enum class eDamperState{Opened, Closed, Opening, Closing, Failure, ClosingF, failClogged};
 //TODO split states for valves, pumps, breakers, DGs
 
 
@@ -336,6 +338,51 @@ class Generator
     void visualize();
     void readBreakersState(bool state1, bool state2);
 
+};
+
+class Damper
+{
+  public:
+    eDamperState damperState = eDamperState::Closed;
+    eDamperState damperPrevState = eDamperState::Closed;
+    mpMode damperMode = Local;
+    uint8_t rgbNumber;
+    Adafruit_PWMServoDriver pwm;
+
+    PCF8574 *pcf1;
+    PCF8574 *pcf2;
+    uint8_t pcf1Pin;
+    uint8_t pcf2Pin;
+
+    unsigned long timer = 0;
+    unsigned long blinkTimer = 0;
+
+    alarmDispsStruct *alarmDisps;
+    mpAlarm damperAlarm1;
+    uint16_t alarmRow = 0;
+
+    RTC_DS1307 *rtc;
+
+  Damper(RTC_DS1307 *_rtc, alarmDispsStruct *_alarmDisps, mpAlarm _damperAlarm1, Adafruit_PWMServoDriver &_pwm, uint8_t _rgbNumber, uint8_t _pcf1Pin, uint8_t _pcf2Pin, PCF8574 *_pcf1 = NULL, PCF8574 *_pcf2 = NULL)
+    {
+      rgbNumber = _rgbNumber;
+      pwm = _pwm;
+      pcf1 = _pcf1;
+      pcf2 = _pcf2;
+      pcf1Pin = _pcf1Pin;
+      pcf2Pin = _pcf2Pin;
+      alarmDisps = _alarmDisps;
+      damperAlarm1 = _damperAlarm1;
+      rtc = _rtc;
+      
+    }
+    
+    void readMode();
+    void readState();
+    void writeCmd();
+    void savePrevState();
+    void closing(uint32_t loadTime);
+    void opening(uint32_t loadTime);
 };
 
 void incrementAlarmCounter();
